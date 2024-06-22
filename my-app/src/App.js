39f960d606b20295481db43483bbf6105a05ef60
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-22 05:10:31 +0000

    removing form contol from the dom to react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ControlledForm from './components/ControlledForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ControlledForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ControlledForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class ControlledForm extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: ''
+      }
+    }
+    handleNameChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+  render() {
+    return (
+      <form>
+        <label htmlFor='id-name'>Your Name</label>
+        <input value={this.state.name}
+        onChange={this.handleNameChange}
+        id='id-name' name='name' 
+        type='text' />
+        <input  type='submit' value={'submit'} />
+
+      </form>
+    )
+  }
+}
+
+export default ControlledForm
